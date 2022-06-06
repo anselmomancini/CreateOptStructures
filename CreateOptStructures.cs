66502ba17017b401b8ddcf9377d6c2c93dcfe32e
Anselmo Mancini <anselmo.mancini@hsl.org.br>
2022-06-05 22:50:03 -0300

    v2 - opt para todos OARs

--- CreateOptStructures.cs
+++ CreateOptStructures.cs
@@ -18,13 +18,15 @@ namespace VMS.TPS
 
         }
 
+        private StructureSet _ss;
+
         public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
         {
             // TODO : Add here your code that is called when the script is launched from Eclipse
 
-            StructureSet ss = context.StructureSet;
+            _ss = context.StructureSet;
 
-            if (ss == null)
+            if (_ss == null)
             {
                 MessageBox.Show("Carregue um paciente e um conjunto de estruturas antes de executar o script");
                 return;
@@ -32,50 +34,60 @@ namespace VMS.TPS
 
             context.Patient.BeginModifications();
 
-            #region encontrar ptv usando foreach e declarando a variável como Structure
-            //Structure ptv = null;
-            //foreach (Structure structure in _ss.Structures)
-            //{
-            //    if (structure.DicomType == "PTV")
-            //    {
-            //        ptv = structure;
-            //        break;
-            //    }
-            //}
-            #endregion
+            var ptvsSum = CreatePtvsSum();
 
-            var ptv = ss.Structures.FirstOrDefault(s => s.DicomType == "PTV");
-
-            if (ptv == null)
+            if (ptvsSum == null)
             {
                 MessageBox.Show("Estrutura 'PTV' não encontrada.");
                 return;
             }
 
-            var reto = ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "reto");
-            if (reto == null)
-                MessageBox.Show("Estrutura 'Reto' não encontrada.");
-            else
+            foreach (var structure in _ss.Structures.Where(s => s.DicomType == "ORGAN").ToList())
             {
-                // fazer sem checar se pode ser adicionada e criando a Structure retoOpt. Na sequência, explicar a necessidade de verificar (fazendo como está na bexiga)
-                Structure retoOpt = ss.AddStructure("CONTROL", "reto_opt");
-                retoOpt.SegmentVolume = reto.Sub(ptv);
-            }
+                if (ThereIsOverlap(structure, ptvsSum))
+                {
+                    if (_ss.CanAddStructure("CONTROL", structure.Id + "_opt"))
+                        _ss.AddStructure("CONTROL", structure.Id + "_opt");
 
-            var bexiga = ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "bexiga");
-            if (bexiga == null)
-                MessageBox.Show("Estrutura 'Bexiga'não encontrada.");
-            else
-            {
-                if (ss.CanAddStructure("CONTROL", "bexiga_opt"))
-                    ss.AddStructure("CONTROL", "bexiga_opt");
+                    _ss.Structures.FirstOrDefault(s => s.Id.ToLower() == structure.Id.ToLower() + "_opt").SegmentVolume = structure.Sub(ptvsSum.Margin(2));
+                }                
+            }           
+        }
 
-                Structure beaxigaOpt = ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "bexiga_opt");
-                beaxigaOpt.SegmentVolume = bexiga.Sub(ptv);
+        private Structure CreatePtvsSum()
+        {
+            List<Structure> ptvs = _ss.Structures.Where(s => s.DicomType == "PTV").ToList();
 
-                // mostrar a possibilidade de:
-                //ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "bexiga_opt").SegmentVolume = bexiga.Sub(ptv);
-            }
+            if (_ss.CanAddStructure("CONTROL", "ptvs_sum"))
+                _ss.AddStructure("CONTROL", "ptvs_sum");
+
+            var ptvsSum = _ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "ptvs_sum");
+
+            ptvsSum.SegmentVolume = ptvs[0].SegmentVolume;
+            for (var i = 1; i < ptvs.Count(); i++)
+                ptvsSum.SegmentVolume = ptvsSum.Or(ptvs[i]);
+
+            return ptvsSum;
+        }
+
+        private bool ThereIsOverlap(Structure st1, Structure st2, double margin = 2)
+        {
+            bool result = false;
+
+            if (_ss.CanAddStructure("CONTROL", "temp"))
+                _ss.AddStructure("CONTROL", "temp");
+
+            var temp = _ss.Structures.FirstOrDefault(s => s.Id.ToLower() == "temp");
+
+            temp.SegmentVolume = st1.And(st2.Margin(margin));
+
+            if (!temp.IsEmpty)
+                result = true;
+
+            if (_ss.CanRemoveStructure(temp))
+                _ss.RemoveStructure(temp);
+
+            return result;
         }
     }
 }
